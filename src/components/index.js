01d96729061010ea1b8deefcd9c3a3baca1a7a0e
AlvaroZ <alvarojzg2@gmail.com>
2025-11-13 01:37:33 -0600

    Creación de los componentes AbutSection, ContactSection, HeroSection, Footer, Navbar, ProjectSection, Configuración del ruter y las notificaciones

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export * from './HeroSection.jsx';
+export * from './AboutSection.jsx';
+export * from './ProjectsSection.jsx';
+export * from './ContactSection.jsx';
+export * from './Navbar.jsx';
+export * from './Footer.jsx';
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
 
+import { Navbar, HeroSection, AboutSection, ContactSection, Footer } from "../components"
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      
+      <Navbar />
+      <main>
+        <HeroSection/>
+        <AboutSection/>
+        <ContactSection/>
+      </main>
+
+      <Footer />
+    </div>
   )
 }
